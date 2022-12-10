97ffd6a99f7ece62babbe9a7ab8222570b136b57
kai <sea.k.naka@gmail.com>
2022-12-10 18:19:01 +0900

    data

--- src/app/puroduct-details/puroduct-details.component.ts
+++ src/app/puroduct-details/puroduct-details.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Product, products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-puroduct-details',
   templateUrl: './puroduct-details.component.html',
   styleUrls: ['./puroduct-details.component.css']
 })
-export class PuroductDetailsComponent {
+export class PuroductDetailsComponent implements OnInit {
+  product: Product | undefined;
 
+  constructor(
+    private route: ActivatedRoute,
+    private cartService :CartService
+  ) { }
+
+  ngOnInit(): void {
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    this.product = products.find(product => product.id === productIdFromRoute);
+  }
+
+  addCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
+  }
 }
